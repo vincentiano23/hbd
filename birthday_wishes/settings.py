da267904b4549acb87188f122c733a61859151e2
vincentiano23 <vincentmuthiani15@gmail.com>
2025-03-28 19:55:45 +0300

    fixed allowed hosts for deployment

--- birthday_wishes/settings.py
+++ birthday_wishes/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-vy$)6=3x(vw$aq8lw1*142bim%=1pd*-84wut8_i(a@m$6m4v&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['happybirthdaytome.onrender.com']
 
 
 # Application definition
